3723b4fd6acc64067e84df12bb351b6fba03b1bd
janojan <github@krupa.pw>
2020-05-31 23:20:01 +0200

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,18 @@
+class profile::ssh_server {
+  package {'openssh-server':
+    ensure => running,
+    enable => true,
+    }
+  
+  service {'sshd':
+    ensure => enabled,
+    }
+    
+  ssh_authorized_key {
+    ensure => present,
+    user => root,
+    type => 'ssh-rsa',
+    key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8bfEJKHjY4ZqJvE6guMOm5ALtw+CcqGM3o9PNBWquhhtZ8LZI81JxFVHeZoWqtI7dclU/a8/jo6aQxp+dQG8Ox/SbQs2AqEOdNqeJpFtxWnR+O/q84A5jIa/yNDF5bKabXgcLXbknZWBw9pVMZA+bUQgTEwdV+GS/bSQb8sZ1i8I4ICpJl59wT8VyZqMihlIIA96Hxyr5RETmYNuP2ip0KS2ygB/sAH3t50IZ6uUqcA6I7J/PWUglgL5C1rlScY5uxJNvahrZQ1S78QmMRxBttS9mCuEnzKGoccO4FUJ1CBMXT7bHnQv0hazI8sLkjz11pdQe7lTh8BeFcIO1za2j root@master.puppet.vm',
+  }
+
+}
